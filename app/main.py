7da0c37b57eb165ceaa1a0f6dfba941bfa03d479
ifeoluwashola <dloveade@gmail.coma>
2025-12-05 13:24:24 +0100

    upgraded to version 2

--- app/main.py
+++ app/main.py
@@ -8,6 +8,6 @@ Instrumentator().instrument(app).expose(app)
 def root():
     return {
         "message": "Hello from DevFest GitOps Demo 🚀", 
-        "version": "v1",
+        "version": "v2",
         "environment": "production"
     }
